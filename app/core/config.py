448f87376b5628dc6fd51e5cb044602cab29ab5b
MisterGold19 <ivan.kuzenkov.public@gmail.com>
2025-10-07 15:10:34 +0200

    again...

--- app/core/config.py
+++ app/core/config.py
@@ -2,13 +2,20 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_DB: str
+    # POSTGRES_USER: str
+    # POSTGRES_PASSWORD: str
+    # POSTGRES_DB: str
+
     ENV: str
     APP_VERSION: str
     LOG_LEVEL: str
     DATABASE_URL: str
+
+    # can be None to run on Github actions
+
+    POSTGRES_USER: str | None = None
+    POSTGRES_PASSWORD: str | None = None
+    POSTGRES_DB: str | None = None
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
